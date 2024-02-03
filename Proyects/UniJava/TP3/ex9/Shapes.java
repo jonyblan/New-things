aad9fd4da9c8960a23df75fd7277f9bc3c87732d
Jony <102042390+jonyblan@users.noreply.github.com>
2024-02-02 21:59:20 -0300

    Continued with UniJava
    Finished TP3/ex9

--- Proyects/UniJava/TP3/ex9/Shapes.java
+++ Proyects/UniJava/TP3/ex9/Shapes.java
@@ -1,44 +1,33 @@
-
+// Done
 public class Shapes {
 	public static void main(String args[]){
 		Rectangle myRectangle = new Rectangle(new Point(0, 0), new Point(2, 4));
 		myRectangle.showRectangle();
-		myRectangle.showPerimeter();
-		myRectangle.showArea();
-		myRectangle.showBase();
-		myRectangle.showHeight();
-	
-		/*Triangle myTriangle = new Triangle(new Point(0, 0), new Point(1, 0), new Point(0, 1));
-		System.out.println(myTriangle);
+		System.out.println(String.format("%.2f", myRectangle.perimeter()));
+		System.out.println(String.format("%.2f", myRectangle.area()));
+		System.out.println(String.format("%.2f", myRectangle.base()));
+		System.out.println(String.format("%.2f", myRectangle.height()));
+
+		Triangle myTriangle = new Triangle(new Point(0, 0), new Point(1, 0), new Point(0, 1));
+		myTriangle.showTriangle();
 		System.out.println(String.format("%.2f", myTriangle.perimeter()));
 		System.out.println(String.format("%.2f", myTriangle.area()));
-	
+
 		Ellipse myEllipse = new Ellipse(new Point(0, 0), 2, 1);
-		System.out.println(myEllipse);
+		myEllipse.showEllipse();
 		System.out.println(String.format("%.2f", myEllipse.perimeter()));
 		System.out.println(String.format("%.2f", myEllipse.area()));
-	
+
 		Circle myCircle = new Circle(new Point(0, 0), 3);
-		System.out.println(myCircle);
+		myCircle.showCircle();
 		System.out.println(String.format("%.2f", myCircle.perimeter()));
-		System.out.println(String.format("%.2f", myCircle.area()));*/
-		
+		System.out.println(String.format("%.2f", myCircle.area()));
 	}
 
-	public static class Shape {
-		private double perimeter, area;
-		public void showArea(){
-			System.out.println("Area: " + area());
-		}
-		public void showPerimeter(){
-			System.out.println("Perimeter: " + perimeter());
-		}
-		public double perimeter(){
-			return this.perimeter;
-		}
-		public double area(){
-			return this.area;
-		}
+
+	public abstract static class Shape {
+		public abstract double perimeter();
+		public abstract double area();
 	}
 
 	public static class Point {
@@ -56,6 +45,26 @@ public class Shapes {
 		public double getY() {
 			return y;
 		}
+
+		public static void showPoint(Point p1){ // should call showPointLast if its the only one of the last one called
+			System.out.print(" { " + p1.x + " , " + p1.y + " } ,");
+		}
+
+		public static void showPointLast(Point p1){ // should call showPointLast if its the only one of the last one called
+			System.out.println(" { " + p1.x + " , " + p1.y + " ]");
+		}
+
+		public static double distanceX(Point p1, Point p2){
+			return Math.abs(p1.x - p2.x);
+		}
+
+		public static double distanceY(Point p1, Point p2){
+			return Math.abs(p1.y - p2.y);
+		}
+
+		public static double distance(Point p1, Point p2){ // inefficient, use distanceX or distanceY if possible
+			return Math.sqrt(Math.pow(distanceX(p1, p2), 2) + Math.pow(distanceY(p1, p2), 2));
+		}
 	}
 
 	public static class Rectangle extends Shape{
@@ -65,16 +74,10 @@ public class Shapes {
 			this.p2 = p2;
 		}
 
-		public Point[] getPoints(){
-			Point[] points = new Point[2];
-			points[0] = p1;
-			points[1] = p2;
-			return points;
-		}
-
 		public void showRectangle(){
-			Point[] points = getPoints();
-			System.out.println("Rectangle [ { " + points[0].getX() + " , " + points[0].getY() + " } , { " + points[1].getX() + " , " + points[1].getY() + " } ]");
+			System.out.print("Rectangle [");
+			Point.showPoint(this.p1);
+			Point.showPointLast(this.p2);
 		}
 
 		public void showBase(){
@@ -86,23 +89,95 @@ public class Shapes {
 		}
 
 		public double base(){
-			return(Math.abs(this.p1.x - this.p2.x));
+			return(Point.distanceX(this.p1, this.p2));
 		}
 
 		public double height(){
-			return(Math.abs(this.p1.y - this.p2.y));
+			return(Point.distanceY(this.p1, this.p2));
+		}
+
+		@Override
+		public double perimeter(){
+			return (Point.distanceX(this.p1, this.p2) * 2 + Point.distanceY(this.p1, this.p2) * 2);
+		}
+
+		@Override
+		public double area(){
+			return (Point.distanceX(this.p1, this.p2) * Point.distanceY(this.p1, this.p2));
 		}
 	}
 
-	/*public class Triangle extends Shape{
+	public static class Triangle extends Shape{
+		private Point p1, p2, p3;
+		public Triangle(Point p1, Point p2, Point p3){
+			this.p1 = p1;
+			this.p2 = p2;
+			this.p3 = p3;
+		}
 
+		public void showTriangle(){
+			System.out.print("Triangle: [");
+			Point.showPoint(this.p1);
+			Point.showPoint(this.p2);
+			Point.showPointLast(this.p3);
+		}
+
+		@Override
+		public double perimeter(){
+			return (Point.distance(this.p1, this.p2) + Point.distance(this.p2, this.p3) + Point.distance(this.p3, this.p1));
+		}
+
+		@Override
+		public double area(){
+			return (0.5 * Math.abs((this.p1.getX() * (this.p2.getY() - this.p3.getY())) + (this.p2.getX() * (this.p3.getY() - this.p1.getY())) + (this.p3.getX() * (this.p1.getY() - this.p2.getY()))));
+		}
 	}
 
-	public class Circle extends Shape{
+	public static class Ellipse extends Shape{
+		private Point center;
+		private double bigger, lower;
+		public Ellipse(Point center, double bigger, double lower){
+			this.center = center;
+			this.bigger = bigger;
+			this.lower = lower;
+		}
 
+		public void showEllipse(){
+			System.out.println("Center: { " + this.center.x + " , " + this.center.y + " } , Diameter1: " + this.bigger + " , Diameter2: " + this.lower);
+		}
+
+		public Point getCenter(){
+			return this.center;
+		}
+
+		public double perimeter(){
+			return (Math.PI * (3 * (this.bigger/2 + this.lower/2) - Math.sqrt((3 * this.bigger/2 + this.lower/2) * (this.bigger/2 + 3 * this.lower/2))));
+		}
+
+		public double area(){
+			return (Math.PI * this.bigger/2 * this.lower/2);
+		}
 	}
 
-	public class Elipse extends Shape{
+	public static class Circle extends Shape{
+		private Point center;
+		private double radius;
 
-	}*/
+		public Circle(Point center, double radius){
+			this.center = center;
+			this.radius = radius;
+		}
+
+		public void showCircle(){
+			System.out.println("Center: { " + this.center.x + " , " + this.center.y + " } , Radius: " + this.radius);
+		}
+
+		public double perimeter(){
+			return (Math.PI * this.radius * 2);
+		}
+
+		public double area(){
+			return (Math.pow(this.radius, 2) * Math.PI);
+		}
+	}
 }
