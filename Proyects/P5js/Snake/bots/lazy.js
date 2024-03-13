e61de65843c01ba0447d93efda1643fe4c1298e1
Jony <102042390+jonyblan@users.noreply.github.com>
2024-03-13 16:24:40 -0300

    Added comments and modularized
    Its no longer a pain to read this code

--- Proyects/P5js/Snake/bots/lazy.js
+++ Proyects/P5js/Snake/bots/lazy.js
@@ -1,65 +1,89 @@
 // the logic of the lazy bot is simple. Just go around the screen in a defined path
-// until you die. It works, but it's slow. I'll do the calculations later
+// until you die. 
+// It works, but it's slow. I'll do the calculations later
 // when I have a bunch of bots set up
 
+function moveUp(){
+	return [0, -1];
+}
 
+function moveDown(){
+	return [0, 1];
+}
+
+function moveLeft(){
+	return [-1, 0];
+}
+
+function moveRight(){
+	return [1, 0];
+}
+
+
+// If you're in the top right corner, go left
 function checkTopRightCorner(headPos, cantSquaresX){
 	let newInstructions = [];
 	
 	if(headPos[0] == cantSquaresX - 1 && headPos[1] == 0){
-		newInstructions[0] = [-1, 0];
+		newInstructions[0] = moveLeft();
 	}
 
 	return newInstructions;
 }
 
+// If you're in the top left corner, go down
 function checkTopLeftCorner(headPos){
 	let newInstructions = [];
 
 	if(headPos[0] == 0 && headPos[1] == 0){
-		newInstructions[0] = [0, 1];
+		newInstructions[0] = moveDown();
 	}
 
 	return newInstructions;
 }
 
+// If you're in the right wall (not the top one) go up and then left
 function checkRightWall(headPos, cantSquaresX){
 	let newInstructions = [];
 
 	if(headPos[0] == cantSquaresX - 1){
-		newInstructions[0] = [0, -1];
-		newInstructions[1] = [-1, 0];
+		newInstructions[0] = moveUp();
+		newInstructions[1] = moveLeft();
 	}
 
 	return newInstructions;
 }
 
+// If you're almost in the left wall (not in the top lane)
+// save space for going down later by going up and then right
 function checkAlmostLeftWall(headPos){
 	let newInstructions = [];
 
 	if(headPos[0] == 1 && headPos[1] != 0){
-		newInstructions[0] = [0, -1];
-		newInstructions[1] = [1, 0];
+		newInstructions[0] = moveUp();
+		newInstructions[1] = moveRight();
 	}
 
 	return newInstructions;
 }
 
-function checkBottomLeftWall(headPos, cantSquaresY){
+// if you're in the bottom left wall, go right
+// (2 times to prevent the checkAlmostLeftWall() function
+// from activating in the next frame)
+function checkBottomLeftCorner(headPos, cantSquaresY){
 	let newInstructions = [];
 
 	if(headPos[0] == 0 && headPos[1] == cantSquaresY - 1){
-		newInstructions[0] = [1, 0];
-		newInstructions[1] = [1, 0];
+		newInstructions[0] = moveRight();
+		newInstructions[1] = moveRight();
 	}
 
 	return newInstructions;
 }
 
-function botTurnLazy(currentInstructions, boardValues){
+// return an array with [xHeadPos, yHeadPos]
+function getHeadPos(boardValues, cantSquaresX, cantSquaresY){
 	let headPos = [];
-	let cantSquaresX = boardValues.length, cantSquaresY = boardValues[0].length;
-	let newInstructions = [];
 
 	for(let i = 0; i < cantSquaresX; i++){
 		for(let j = 0; j < cantSquaresY; j++){
@@ -70,19 +94,35 @@ function botTurnLazy(currentInstructions, boardValues){
 		}
 	}
 
+	return headPos;
+}
+
+function areInstructionsClear(newInstructions, currentInstructions){
+	return(	
+			(newInstructions === undefined ||
+			newInstructions.length == 0) && 
+			(currentInstructions === undefined || 
+				currentInstructions.length == 0));
+}
+
+function botTurnLazy(currentInstructions, boardValues){
+	let cantSquaresX = boardValues.length, cantSquaresY = boardValues[0].length;
+	let headPos = getHeadPos(boardValues, cantSquaresX, cantSquaresY);
+	let newInstructions = [];
+
 	newInstructions = checkTopRightCorner(headPos, cantSquaresX);
 
-	if(newInstructions.length == 0){
+	if(areInstructionsClear(newInstructions, currentInstructions)){
 		newInstructions = checkTopLeftCorner(headPos);
 	}
-	if(newInstructions.length == 0 && (currentInstructions === undefined || currentInstructions.length == 0)){
+	if(areInstructionsClear(newInstructions, currentInstructions)){
 		newInstructions = checkRightWall(headPos, cantSquaresX);
 	}
-	if(newInstructions.length == 0 && (currentInstructions === undefined || currentInstructions.length == 0)){
+	if(areInstructionsClear(newInstructions, currentInstructions)){
 		newInstructions = checkAlmostLeftWall(headPos);
 	}
-	if(newInstructions.length == 0){
-		newInstructions = checkBottomLeftWall(headPos, cantSquaresY);
+	if(areInstructionsClear(newInstructions, currentInstructions)){
+		newInstructions = checkBottomLeftCorner(headPos, cantSquaresY);
 	}
 
 	return newInstructions;
